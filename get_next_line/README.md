707ae4e7125b357750452167aae306f6883d3009
Aphilome <1.katrin.1@mail.ru>
2022-01-16 18:05:32 +0300

    gnl

--- get_next_line/README.md
+++ get_next_line/README.md
@@ -3,4 +3,4 @@
 This project is about programming a function that returns a line
 read from a file descriptor.
 
-gnl-war-machine-v2019, main.c, test.txt - testers
+gnl-war-machine-v2019, 1gnlTester, gnlTester, main.c, test.txt - testers
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -2,27 +2,38 @@
 #include <unistd.h>
 #include <sys/fcntl.h>
 #include "get_next_line.h"
+#include "stdlib.h"
 
-int main()
-{
-	int fd = open("../test.txt", O_RDONLY);
-	char *s;
-	int status;
-	printf("==================================\n");
-	status = get_next_line(fd, &s);
-	while(status == 1)
-	{
-		printf("%s|\n", s);
-		free(s);
-		status = get_next_line(fd, &s);
-	}
-	if (status == 0)
-	{
-		printf("%s&\n", s);
-		free(s);
-		status = get_next_line(fd, &s);
-	}
-	printf("==================================\n");
-	close(fd);
-	return 0;
-}
+ int main()
+ {
+ 	int fd = open("../text.txt", O_RDONLY);
+ 	char *s;
+ 	int status;
+ 	printf("==================================\n");
+ 	status = get_next_line(fd, &s);
+ 	while(status == 1)
+ 	{
+ 		printf("%s|\n", s);
+ 		free(s);
+ 		status = get_next_line(fd, &s);
+ 	}
+ 	if (status == 0)
+ 	{
+ 		printf("%s&\n", s);
+ 		free(s);
+ 		status = get_next_line(fd, &s);
+ 	}
+ 	printf("==================================\n");
+ 	close(fd);
+	//while (1);
+ 	return 0;
+ }
+//int	main(int argc, char *argv[])
+//{
+//	int *m = (int *) malloc(10 * sizeof (int));
+//	for (int i = 0; i < 10; i++)
+//	{
+//		m[i] = i;
+//	}
+//	return 0;
+//}
