c4bd3a0341f0a323452380bfa47f1b859b4481e5
Tion Birdperson <tbirdper@ev-i3.kzn.21-school.ru>
2022-03-09 16:14:49 +0300

    norm

--- cub3d/utils/numbers.c
+++ cub3d/utils/numbers.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   numbers.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbirdper <tbirdper@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 16:14:39 by tbirdper          #+#    #+#             */
+/*   Updated: 2022/03/09 16:14:39 by tbirdper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "utils.h"
 
-int ft_max(int a, int b)
+int	ft_max(int a, int b)
 {
 	if (a >= b)
 		return (a);
 	return (b);
 }
 
-int ft_min(int a, int b)
+int	ft_min(int a, int b)
 {
 	if (a <= b)
 		return (a);
